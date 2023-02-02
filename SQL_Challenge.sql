765a26259b0161ab738789b7325bc982f8ba3a58
Abhishek Soni <117580207+Abhis0ni@users.noreply.github.com>
2023-02-03 00:12:27 +0530

    Update SQL_Challenge.sql

--- SQL_Challenge.sql
+++ SQL_Challenge.sql
@@ -20,4 +20,26 @@
     union all 
     Select CITY,length(CITY) as "City_length" from STATION order by length(CITY) desc,CITY asc limit 1;
     
-11) 
+11) Select distinct CITY from STATION where Left(CITY,1) in ('a','e','i','o','u');
+
+12) Select distinct CITY from STATION where Right(CITY,1) in ('a','e','i','o','u');
+
+13) Select distinct CITY from STATION where Left(CITY,1) not in ('a','e','i','o','u');
+
+14) Select distinct CITY from STATION where Right(CITY,1) not in ('a','e','i','o','u');
+
+15) Select distinct CITY from STATION where (Right(CITY,1) not in ('a','e','i','o','u')) or (Left(CITY,1) not in ('a','e','i','o','u'));
+
+16) Select distinct CITY from STATION where (Right(CITY,1) not in ('a','e','i','o','u')) and (Left(CITY,1) not in ('a','e','i','o','u'));
+
+17) Select P.product_id,P.product_name from Product P inner join Sales S 
+    on P.product_id=S.product_id
+    where S.sale_date between '20190101' and '20210331';
+    
+18) Select distinct auther_id as id from Views where auther_id=viewer_id order by auther_id;
+
+19) Select (count(*)/(Select count(*) from Delivery)*100) as "Immediate_percentage" from Delivery where order_date=customer_pref_delivery_date;
+
+20) Select ad_id, (case when Clicked+Viewed=0 then 0 else (Clicked/(Clicked+Viewed))*100) as CTR 
+    (Select ad_id,(Select count(*) from Ads where action='Clicked' and ad_id=A.ad_id) as "Clicked",
+    (Select count(*) from Ads where action='Viewed' and ad_id=A.ad_id) as "Viewed" from Ads A)
